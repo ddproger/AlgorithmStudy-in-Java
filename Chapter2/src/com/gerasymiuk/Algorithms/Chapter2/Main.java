a4ff3725649e49b0e73ab51143b9fc3f5939035f
dgerasymiuk <d.gerasymiuk@gmail.com>
2017-10-30 21:01:56 +0200

    create project

---
+++ Chapter2/src/com/gerasymiuk/Algorithms/Chapter2/Main.java
@@ -0,0 +1,10 @@
+package com.gerasymiuk.Algorithms.Chapter2;
+
+/**
+ * Created by gerasymiuk on 30.10.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Chapter2/src/com/gerasymiuk/Algorithms/Chapter2/SearchServices/LinearSearch.java
@@ -0,0 +1,7 @@
+package com.gerasymiuk.Algorithms.Chapter2.SearchServices;
+
+/**
+ * Created by gerasymiuk on 30.10.17.
+ */
+public class LinearSearch {
+}
