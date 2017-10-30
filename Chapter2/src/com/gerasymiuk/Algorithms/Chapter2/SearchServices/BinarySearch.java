7b28d5e0d23a8b585f3764a6485ded11ef8c85d5
dgerasymiuk <d.gerasymiuk@gmail.com>
2017-10-30 21:15:15 +0200

    add Binary Search class correct .gitignore

--- Chapter2/src/com/gerasymiuk/Algorithms/Chapter2/SearchServices/BinarySearch.java
+++ Chapter2/src/com/gerasymiuk/Algorithms/Chapter2/SearchServices/BinarySearch.java
@@ -1,10 +1,7 @@
 package com.gerasymiuk.Algorithms.Chapter2.SearchServices;
 
-import java.util.TreeSet;
-
 /**
  * Created by gerasymiuk on 30.10.17.
  */
-public class LinearSearch<T extends Comparable> {
-
+public class BinarySearch {
 }
